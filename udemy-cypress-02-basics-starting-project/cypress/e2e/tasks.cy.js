c58ff65c7921a30d1391c2bd9da51975523471f5
Ronald Gerits <ronald.gerits@gmail.com>
2023-05-21 17:13:35 +0200

    Commit node modules

---
+++ udemy-cypress-02-basics-starting-project/cypress/e2e/tasks.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+describe("tasks management", () => {
+  it("should open and close the modal", () => {
+    cy.visit("http://localhost:5173");
+    cy.contains("Add Task").click();
+    cy.get(".backdrop").click({ force: true });
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+    cy.contains("Add Task").click();
+    cy.contains("Cancel").click();
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+  });
+});
--- udemy-cypress-02-basics-starting-project/src/App.jsx
+++ udemy-cypress-02-basics-starting-project/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Modal from './components/Modal';
+import { useState } from "react";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
 
-import NewTask from './components/NewTask';
-import TaskControl from './components/TaskControl';
-import TaskList from './components/TaskList';
+import NewTask from "./components/NewTask";
+import TaskControl from "./components/TaskControl";
+import TaskList from "./components/TaskList";
 
 function App() {
   const [isAddingTask, setIsAddingTask] = useState(false);
   const [tasks, setTasks] = useState([]);
-  const [appliedFilter, setAppliedFilter] = useState('all');
+  const [appliedFilter, setAppliedFilter] = useState("all");
 
   const displayedTasks = tasks.filter((task) => {
-    if (appliedFilter === 'all') {
+    if (appliedFilter === "all") {
       return true;
     }
     return task.category === appliedFilter;
