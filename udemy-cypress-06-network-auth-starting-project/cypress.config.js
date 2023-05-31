70e1d38a0b467f6c22368f3550a7192217ace698
Ronald Gerits <ronald.gerits@gmail.com>
2023-05-31 23:31:46 +0200

    WIP lecture 06

--- udemy-cypress-06-network-auth-starting-project/cypress.config.js
+++ udemy-cypress-06-network-auth-starting-project/cypress.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
-import { seed } from './prisma/seed-test';
+import { seed } from "./prisma/seed-test";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('task', {
+      on("task", {
         async seedDatabase() {
           await seed();
           return null;
-        }
-      })
+        },
+      });
     },
   },
 });
